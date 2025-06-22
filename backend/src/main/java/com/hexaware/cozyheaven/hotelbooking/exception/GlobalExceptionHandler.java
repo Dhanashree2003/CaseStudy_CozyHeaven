f647ab81e25561f75aca1a37a35b2be83886ad76
Dhanashree2003 <dhanashree.r2021csbs@sece.ac.in>
2025-06-22 21:39:30 +0530

    backend

--- backend/src/main/java/com/hexaware/cozyheaven/hotelbooking/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/hexaware/cozyheaven/hotelbooking/exception/GlobalExceptionHandler.java
@@ -1,22 +1,22 @@
-// package com.hexaware.cozyheaven.hotelbooking.exception;
+package com.hexaware.cozyheaven.hotelbooking.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// @RestControllerAdvice
-// public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(ResourceNotFoundException.class)
-//    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
-//        return new ResponseEntity<>("Resource Not Found: " + ex.getMessage(), HttpStatus.NOT_FOUND);
-//    }
+   @ExceptionHandler(ResourceNotFoundException.class)
+   public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+       return new ResponseEntity<>("Resource Not Found: " + ex.getMessage(), HttpStatus.NOT_FOUND);
+   }
 
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> handleGenericException(Exception ex) {
-//        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-// }
+   @ExceptionHandler(Exception.class)
+   public ResponseEntity<String> handleGenericException(Exception ex) {
+       return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+   }
+}
 
 
